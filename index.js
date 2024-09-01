d3a46ac883cd4894d9455615b7420a820f9a860e
uthaya23 <113130843+uthaya23@users.noreply.github.com>
2024-09-01 19:23:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,6 @@ const app=express()
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
-const corsOptions = {
-    origin: 'https://marvelous-syrniki-cadeb8.netlify.app', // Your frontend domain
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-app.use(cors(corsOptions));
 app.use(cors({
     origin: 'http://localhost:3000', // Update this to your frontend URL if needed
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
